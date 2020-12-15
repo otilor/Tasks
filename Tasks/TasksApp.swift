706a6dc88ad4abcc8225713f9f382aed8a3973c1
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-12-15 07:57:00 +0100

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Gabriel Akinyosoye on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
