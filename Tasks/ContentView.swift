0e8706aa4115c0fc146338d40d898b803b662117
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-12-15 10:45:55 +0100

    Add new button with icon

--- Tasks/ContentView.swift
+++ Tasks/ContentView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var shortString = true
     var body: some View {
-        Text("Welcome to my first iOS application.")
-            .padding().foregroundColor(.blue)
-        
-        Text("Designed by Akinyosoye Gabriel").foregroundColor(.red)
+        VStack {
+                    if shortString {
+                        Text("Welcome to Tasks").font(.title).fixedSize()
+                        .transition(AnyTransition.opacity.animation(.easeInOut(duration:1.0)))
+                    }
+                    if !shortString {
+                        Text("by Gabriel Akinyosoye").font(.title).fixedSize()
+                            .transition(AnyTransition.opacity.animation(.easeInOut(duration:1.0)))
+                    }
+
+                    Button(action: {self.shortString.toggle()}) {
+                        Text("Toggle")
+                            .padding()
+                    }
+                    .accentColor(.white)
+                    .background(Color .gray)
+                    .cornerRadius(10.0)
+                    .padding()
+            
+            
+            Button(action: {}) {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+            }
+            .padding()
+            .accentColor(.red)
+            .foregroundColor(.white)
+            .background(Color.red)
+            .cornerRadius(10)
     }
 }
 
@@ -21,4 +49,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
 }
